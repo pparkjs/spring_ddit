c480bf7d06ec2d92ad3b7f3d2a51b2a4baa2cb5c
pparkjs <qkrwjdtn741@naver.com>
2023-05-16 17:59:06 +0900

    Book 테이블의 service 추가

---
+++ SampleSpringYse/src/main/java/kr/or/ddit/book/service/BookService.java
@@ -0,0 +1,8 @@
+package kr.or.ddit.book.service;
+
+import java.util.Map;
+
+public interface BookService {
+	public String insertBook(Map<String, Object> map);
+	public Map<String, Object> selectBook(Map<String, Object> map);
+}
