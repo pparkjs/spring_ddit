b886dfc6766fdc307ac4e1c66ba3cc2a27a280ea
pparkjs <qkrwjdtn741@naver.com>
2023-05-16 17:59:42 +0900

    Book 등록하기 메소드 추가

--- SampleSpringYse/src/main/java/kr/or/ddit/book/web/BookInsertController.java
+++ SampleSpringYse/src/main/java/kr/or/ddit/book/web/BookInsertController.java
@@ -1,20 +1,34 @@
 package kr.or.ddit.book.web;
 
+import java.util.Map;
+
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
-@RequestMapping("/book")
-public class BookInsertController {
+import kr.or.ddit.book.service.BookService;
 
 /*
  * @Controller
 	- 어노테이션에 있는 클래스는 스프링이 브라우저의 요청(request)을 받아들이는 컨트롤러라고 인지해서 자바 빈(Java Bean)으로 등록해서 관리한다.
 	- 여기서 자바 빈이란 객체를 만들어서 메모리에 올리는 형태를 말한다.
  */
-	@RequestMapping(value="/form.do", method = RequestMethod.GET)
+@Controller
+@RequestMapping("/book")
+public class BookInsertController {
+	
+	
+	/*
+	 * 서비스를 호출하기 위해 BookService를 의존성을 주입한다.
+	 * 의존성 주입을 통한 결합도 낮추기 
+	 */
+	@Inject
+	private BookService bookService;
+	
 	
 	/*
 	 * @RequestMapping
@@ -31,8 +45,33 @@ public class BookInsertController {
 		
 		뷰의 경로를 ‘book/form’과 같은 형태로 전달하는 이유는 요청(request)에 해당하는 url의 mapping 되는 화면의 경로 값을 뒤에는 ‘.jsp’가 붙어 최종 위치에 해당하는 jsp 파일을 찾아준다.
 	 */
+	@RequestMapping(value="/form.do", method = RequestMethod.GET)
 	public ModelAndView bookForm() {
 		//return new ModelAndView(”redirect:/book/form.do”); redirect 방식
 		return new ModelAndView("book/form");//forward라고 할 수 있다.
 	}
-}
+	
+	/*
+	 *  데이터의 변경이 일어나므로 http메소드는 POST방식으로 처리
+	 *  @RequestParam은 HTTP 파라미터를 map변수에 자동으로 바인딩한다.
+	 *  Map 타입의 경우는 @RequestParam을 붙여야만 HTTP 파라미터의 값을 map안에 바인딩해준다.
+	 */
+	@RequestMapping(value="/form.do", method = RequestMethod.POST)
+	public ModelAndView insertBook(@RequestParam Map<String, Object> map) {
+		ModelAndView mav = new ModelAndView();
+		// 서비스 메소드 insertBook를 호출하여 책을 등록한다.
+		// 서비스 메소드 insertBook을 통해서 책을 등록하고 결과로 bookId를 리턴 받아온다.
+		String bookId = bookService.insertBook(map);
+		if(bookId == null) {
+			// 데이터 입력이 실패한 경우 다시 데이터를 입력받아야 하므로 생성 화면으로  redirect한다
+			// ModerAndView 객체는 .setViewName 메소드를 통해 뷰의 경로를 지정할 수 있다.
+			mav.setViewName("redirect:/book/form.do");
+			// 뷰의 경로가 redirect: 로 시작하면 스프링은 뷰 파일을 찾아가는게 아니라
+			// 웹 페이지의 주소(/form.do)를 찾아간다.
+		}else {
+			// 데이터 입력이 성공하면 상세 페이지로 이동한다.
+			mav.setViewName("redirect:/book/detail.do?bookId=" + bookId);
+		}
+		return mav;
+	}
+ }
