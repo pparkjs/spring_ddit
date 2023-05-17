cd8da6a5161c8ef17a87b44dd4ade31b95567bef
pparkjs <qkrwjdtn741@naver.com>
2023-05-17 18:24:02 +0900

    BookService의 interface

--- SampleSpringYse/src/main/java/kr/or/ddit/book/service/BookService.java
+++ SampleSpringYse/src/main/java/kr/or/ddit/book/service/BookService.java
@@ -1,8 +1,14 @@
 package kr.or.ddit.book.service;
 
+import java.util.List;
 import java.util.Map;
+
+import org.springframework.web.servlet.ModelAndView;
 
 public interface BookService {
 	public String insertBook(Map<String, Object> map);
 	public Map<String, Object> selectBook(Map<String, Object> map);
+	public boolean updateBook(Map<String, Object> map);
+	public boolean removeBook(Map<String, Object> map);
+	public List<Map<String, Object>> selectBookList(Map<String, Object> map);
 }
