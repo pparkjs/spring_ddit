9631760f826674e96da1d82357c1dce3207e162b
pparkjs <qkrwjdtn741@naver.com>
2023-05-19 21:07:15 +0900

    일반게시판 dao

---
+++ FirstSpring/src/main/java/kr/or/ddit/board/dao/IBoardDAO.java
@@ -0,0 +1,18 @@
+package kr.or.ddit.board.dao;
+
+import java.util.List;
+
+import kr.or.ddit.vo.BoardVO;
+import kr.or.ddit.vo.PaginationInfoVO;
+
+public interface IBoardDAO {
+	
+	public int insertBoard(BoardVO boardVO);
+	public void incrementHit(int boNo);
+	public BoardVO selectBoard(int boNo);
+	public int updateBoard(BoardVO boardVO);
+	public int deleteBoard(int boNo);
+	public List<BoardVO> selectBoardList();
+	public int selectBoardCount(PaginationInfoVO<BoardVO> pagingVO);
+	public List<BoardVO> selectBoardBoardList2(PaginationInfoVO<BoardVO> pagingVO);
+}
